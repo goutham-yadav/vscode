9787a7ef8924e350cc2bd41b3ac1ea4212d8c17b
goutham-yadav <rgoutham259@gmail.com>
2025-03-06 15:03:57 +0530

    ECOMERCE WEBPAGE

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("you have entered")
+<script>
+        document.querySelector('.shop-now').addEventListener('click', function() {
+            window.location.href = '#products';
+        });
+
+        document.querySelectorAll('.add-to-cart').forEach(button => {
+            button.addEventListener('click', function() {
+                alert('Item added to cart!');
+            });
+        });
+    </script>git 
